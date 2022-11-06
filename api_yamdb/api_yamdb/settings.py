7649b109b25f26fd1c149a3d35b7ce0035d19cb8
Julius Theodore <autodeterminatio@gmail.com>
2022-11-06 14:26:01 +0300

    ревью_1_без_ssl

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -11,7 +11,7 @@ DEBUG = os.getenv('DEBUG', False)
 
 # ALLOWED_HOSTS = ['*']
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='HOST')
 
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
